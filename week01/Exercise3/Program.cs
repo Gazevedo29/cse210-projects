299433ac6c55d0b5b284fe7049814adbb9151136
Gerson Azevedo <gazevedo@byupathway.edu>
2026-09-05 14:32:14 +0200

    exercicio 4

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        Console.Write("What is your Guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+
+        if (guess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("Congratulations! You guessed the magic number!");
+        }
+
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your Guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You guessed the magic number!");
+            }
+        }
     }
 }
