76b0b5a9df693a90433e58256962285a0c7f5ab9
Gerson Azevedo <gazevedo@byupathway.edu>
2026-09-05 15:09:24 +0200

    exercicio 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,48 @@ using System;
 
 class Program
 {
+    // O ponto de entrada principal do programa
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int square = SquareNumber(number);
+        DisplayResult(name, square);
+    }
+
+    // Exibe a mensagem de boas-vindas
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    // Pede o nome do usuário
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    // Pede o número favorito do usuário
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    // Calcula o quadrado do número
+    static int SquareNumber(int number)
+    {
+        int squared = number * number;
+        return squared;
+    }
+
+    // Exibe o resultado final
+    static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.WriteLine($"{name}, the square of your favorite number is {squaredNumber}.");
     }
 }
